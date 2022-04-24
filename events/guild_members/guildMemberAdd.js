4ddb9ec886e859054c7662d8bc3b35ff47126bf2
Seïtan_Mëitoo <82282083+seitanmeitoo@users.noreply.github.com>
2022-04-24 18:15:54 +0000

    Modification des événements guildMemberAdd et guildMemberRemove pour qu'ils envoient des messages d'information de l'utilisateur dans un channel spécifique

--- events/guild_members/guildMemberAdd.js
+++ events/guild_members/guildMemberAdd.js
@@ -1,7 +1,26 @@
+const { MessageEmbed, Formatters } = require('discord.js');
+const dayjs = require('dayjs');
+
 module.exports = {
   name: 'guildMemberAdd',
   once: false,
   async execute(client, member) {
-    console.log('test');
+    const creationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
+    const relativeCreationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
+    const joinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
+    const relativeJoinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
+    
+    const embed = new MessageEmbed()
+      .setAuthor({ name: `${member.user.tag} (${member.id})`, iconURL: member.user.displayAvatarURL() })
+      .setColor('#21ff81')
+      .setDescription(`± Nom d'utilisateur : ${member} - \`${member.user.tag}\` (${member.id})\n
+        ± A créer son compte le : ${creationTimestamp} (${relativeCreationTimestamp})\n
+        ± A rejoint le : ${joinTimestamp} (${relativeJoinTimestamp})`)
+      .setTimestamp()
+      .setFooter({ text: 'Un utilisateur viens rejoindre le serveur !' });
+    
+    const logChannel = client.channels.cache.get('958787301935300658');
+
+    logChannel.send({ embeds: [embed] });
   },
 };
--- events/guild_members/guildMemberRemove.js
+++ events/guild_members/guildMemberRemove.js
@@ -1,7 +1,29 @@
+const { MessageEmbed, Formatters } = require('discord.js');
+const dayjs = require('dayjs');
+
 module.exports = {
   name: 'guildMemberRemove',
   once: false,
   async execute(client, member) {
-    console.log('test');
+    const creationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
+    const relativeCreationTimestamp = Formatters.time(dayjs(member.user.createdTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
+    const joinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.ShortDateTime);
+    const relativeJoinTimestamp = Formatters.time(dayjs(member.joinedTimestamp).unix(), Formatters.TimestampStyles.RelativeTime);
+    const leaveTimestamp = Formatters.time(dayjs().unix(), Formatters.TimestampStyles.ShortDateTime);
+    const relativeLeaveTimestamp = Formatters.time(dayjs().unix(), Formatters.TimestampStyles.RelativeTime);
+    
+    const embed = new MessageEmbed()
+      .setAuthor({ name: `${member.user.tag} (${member.id})`, iconURL: member.user.displayAvatarURL() })
+      .setColor('#dc143c')
+      .setDescription(`± Nom d'utilisateur : ${member} - \`${member.user.tag}\` (${member.id})\n
+        ± A créer son compte le : ${creationTimestamp} (${relativeCreationTimestamp})\n
+        ± A rejoint le : ${joinTimestamp} (${relativeJoinTimestamp})\n
+        ± A quitté le : ${leaveTimestamp} (${relativeLeaveTimestamp})`)
+      .setTimestamp()
+      .setFooter({ text: 'Un utilisateur viens quitter le serveur !' });
+    
+    const logChannel = client.channels.cache.get('958787301935300658');
+
+    logChannel.send({ embeds: [embed] });
   },
 };
